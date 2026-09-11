fa1b3cc8ae4a060b5740a998a72ca71fc2570958
Celeste Aguilar <celesteaguilar29@gmail.com>
2026-09-11 14:33:03 -0600

    feat: datos del listado de componentes e inicializacion de proyecto

---
+++ src/data/elementos.js
@@ -0,0 +1,14 @@
+export const elementos = [
+  { id: 1, nombre: 'Procesador Intel Xeon E-2388G', categoria: 'Procesadores', valor: 650.00 },
+  { id: 2, nombre: 'Procesador AMD EPYC 7763', categoria: 'Procesadores', valor: 3200.00 },
+  { id: 3, nombre: 'Procesador Intel Core i9-14900K', categoria: 'Procesadores', valor: 580.00 },
+  { id: 4, nombre: 'Memoria RAM DDR5 32GB ECC Server', categoria: 'Memorias RAM', valor: 210.00 },
+  { id: 5, nombre: 'Memoria RAM DDR4 16GB Kingston', categoria: 'Memorias RAM', valor: 65.00 },
+  { id: 6, nombre: 'Memoria RAM DDR5 64GB Corsair', categoria: 'Memorias RAM', valor: 340.00 },
+  { id: 7, nombre: 'Disco SSD NVMe Enterprise 2TB', categoria: 'Almacenamiento', valor: 280.00 },
+  { id: 8, nombre: 'Disco Duro SAS 10TB Seagate', categoria: 'Almacenamiento', valor: 310.00 },
+  { id: 9, nombre: 'Disco SSD SATA 960GB Kingston', categoria: 'Almacenamiento', valor: 110.00 },
+  { id: 10, nombre: 'Tarjeta de Red 10GbE Dual Port', categoria: 'Redes', valor: 195.00 },
+  { id: 11, nombre: 'Switch Administrable 24 Puertos Gigabit', categoria: 'Redes', valor: 450.00 },
+  { id: 12, nombre: 'Firewall Hardware OPNsense Appliance', categoria: 'Redes', valor: 890.00 }
+];
